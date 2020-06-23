a2cfce00cb5a998e6547baea100e0bff7b37f062
kazuyamano <ymkzjn@gmail.com>
2020-06-24 06:16:24 +0900

    datetimeJST変換修正

--- main/models.py
+++ main/models.py
@@ -6,8 +6,8 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     jcode = db.Column(db.String)
     temp = db.Column(db.Float)
-    JST = datetime.now(timezone(timedelta(hours=9)))
-    date = db.Column(db.DateTime, default= JST)
+    JST = timezone(timedelta(hours=+9), 'JST')
+    date = db.Column(db.DateTime, default= datetime.now(JST))
 
     def __repr__(self):
         return "<Entry id={} jcode={!r} temp={!r} date={}>".format(self.id, self.jcode, self.temp, self.date)
