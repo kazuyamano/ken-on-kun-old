116d860659c0f293d36fdf441c6df75ab940f4aa
kazuyamano <ymkzjn@gmail.com>
2020-07-22 15:52:06 +0900

    送信完了画面を新設、社員コードで文言・履歴出し分け

--- main/views.py
+++ main/views.py
@@ -1,23 +1,46 @@
 import flask 
+from flask import request
 from main import app, db
 from main.models import Entry
 from sqlalchemy import desc
 
-@app.route('/')
+@app.route('/', methods=['get'])
 def show_entries():
-    entries = Entry.query.all() 
-        #Entry  テーブル名
-        #.query クエリ（指示）
-        #.all() 全件取得
-        #別の書き方: session.query(Entry).all()
-    entries_head = Entry.query.order_by(desc(Entry.date)).limit(5).all()
-        #.order_by()    並び順を指定
-        #desc(Entry.date)   'date'カラムの降順
-    return flask.render_template('entries.html', entries=entries, entries_head=entries_head)
+    entries_head = Entry.query\
+        .order_by(desc(Entry.date))\
+        .limit(5)\
+        .all()
+    return flask.render_template('entry.html', entries_head=entries_head)
 
-@app.route('/add', methods=['POST'])
+@app.route('/added', methods=['POST'])
 def add_entry():
     entry = Entry(jcode = flask.request.form['jcode'],temp = flask.request.form['temp'])
     db.session.add(entry)
     db.session.commit()
-    return flask.redirect(flask.url_for('show_entries'))
+    added_jcode = request.form.get('jcode')
+    entries_jcode = db.session.query(Entry)\
+        .filter(Entry.jcode==added_jcode)\
+        .order_by(desc(Entry.date))\
+        .all()
+    return flask.render_template('added.html', added_jcode=added_jcode, entries_jcode=entries_jcode)
+
+@app.route('/logs', methods=['post','get'])
+def view_logs():
+  df = pd.read_csv('static/BodyTemp_data.csv',index_col=0)
+  df_columns = df.columns.tolist()
+  df_columns.insert(0,'date')
+  df_values = df.values.tolist()
+  df_index = df.index.tolist()
+  return render_template('logs.html', title='履歴を見る画面どすえ', df_columns=df_columns, df_values=df_values, df_index=df_index)
+
+
+
+
+# SQLクエリの書き方
+# 本来の記述はsessionを使う。           db.session.query(Entry).all()
+# 'Entry'クラスが持つヘルパー機能のため   Entry.query.all() 　に省略可能
+#   'Entry'         クラス名＝テーブル名
+#   .query          クエリ（指示）
+#   .order_by()     並び順を指定
+#   desc(Entry.date)    'date'カラムの降順で
+#   .all()          全件取得
